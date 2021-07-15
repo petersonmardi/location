2954f820fa5e1d8b52f92d809a80cbf18fd9eb87
petersonmardi <petersonmardi@gmail.com>
2021-07-14 20:08:51 -0400

    Add event handler

--- location/static/js/app.js
+++ location/static/js/app.js
@@ -1,1 +1,23 @@
-$(".pt-hide").hide(400);
+$(".pt-form-adding-client").hide();
+
+$(".pt-btn-add").on("click",
+   function() {
+     $(".pt-form-adding-client").show(400);
+   });
+
+$(".pt-submit-adding-client").on("click",
+   function() {
+     $(".pt-form-adding-client").hide(400);
+   });
+
+$(".pt-form-updating-client").hide();
+
+$(".pt-edit-client").on("click",
+   function() {
+     $(".pt-form-updating-client").show(400);
+   });
+
+$(".pt-submit-updating-client").on("click",
+   function() {
+     $(".pt-form-updating-client").hide(400);
+   });
